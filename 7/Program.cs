b4f7d934828f30e6d23e17abbff57eb2317b0943
IrinaSavosh <irina_savosh@mail.ru>
2022-11-24 23:08:09 +0300

    experimented before

---
+++ 7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программ, которая принмает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+// 456->6
+// 458->8
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+   Console.WriteLine(number % 10);
+}
+else
+{
+  Console.WriteLine("Введено не трехзначное число");
+}

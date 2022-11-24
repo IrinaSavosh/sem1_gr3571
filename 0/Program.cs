0334ecad4d0c7c415e90ff5aca9fe7e8fe5a3034
IrinaSavosh <irina_savosh@mail.ru>
2022-11-24 23:25:21 +0300

    added magic

---
+++ 0/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number * number);
--- 2/Program.cs
+++ 2/Program.cs
@@ -2,6 +2,7 @@
 // a = 5; b = 7 -> max = 7, min = 5
 // a = 2 b = 10 -> max = 10, min = 2
 // a = -9 b = -3 -> max = -3, min = -9
+Console.WriteLine("Начнем творить магию!");
 Console.WriteLine("Введите число a:");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b:");

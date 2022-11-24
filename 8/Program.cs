471b3f4ec3c97057e63199b65a40d1608e807ca0
IrinaSavosh <irina_savosh@mail.ru>
2022-11-24 23:10:55 +0300

    task: even numbers

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число больше 1");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= number; i += 2)
+{
+   Console.WriteLine(i);
+}
+Console.WriteLine("Вот и магии конец)))");

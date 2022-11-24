fea75132cf748b143f2d3220ba0c535f5b207a84
IrinaSavosh <irina_savosh@mail.ru>
2022-11-24 23:18:42 +0300

    task: root of a number

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, 
+// которая на вход принимает двачисла и проверяет, является ли
+//первоечислоквадратом вторго
+
+//а = 25, b = 5  - да
+// а = 2, b = 10  - нет
+// а = 9, b = 3  - да
+//а = 8, b = 6  - нет
+
+
+Console.WriteLine("Введите число a:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB*numberB)
+{
+   Console.WriteLine("Да!");
+}
+else
+{
+   Console.WriteLine("Нет!");
+}
